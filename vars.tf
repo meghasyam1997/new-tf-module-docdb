0ea5a792c6d7a315f6f1bd47f6a0065861b0fe73
meghasyam1997 <meghasyam01@gmail.com>
2023-09-22 16:25:22 +0530

    creating docdb

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default = "docdb"
+}
+variable "port_no" {
+  default = 27017
+}
+variable "env" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "instance_count" {}
+variable "instance_class" {}
+variable "engine_version" {}
+variable "subnets" {}
+variable "kms_arn" {}
